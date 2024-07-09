5ad98a6124e70e20a8147bb69032107b876544e8
tjonesutd87 <tjonesutd87@gmail.com>
2024-07-08 23:46:52 -0500

    fix syntax, fix functions

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 
-const pageWidth = 800;
-const pageHeight = 600;
+const canvasWidth = 800;
+const canvasHeight = 800;
 
+let canvas = document.getElementById("canvas");
+canvas.style.width = `${canvasWidth}px`
+canvas.style.height = `${canvasHeight}px`
 function createGrid (num) {
    const grid = num * num;
-   let canvas = document.getElementById("canvas");
-   for (i= 0; i <= grid; i++){
+   const cellWidth = canvasWidth / num;
+   const cellHeight = canvasHeight / num;
+   for (i= 1; i <= grid; i++){
       let cell = document.createElement("div");
-      cell.addAttribute("class", "cell");
+      cell.setAttribute("class", "cell");
+      cell.style.width = `${cellWidth}px`;
+      cell.style.height = `${cellHeight}px`;
+      cell.style.opacity = "10%";
       canvas.appendChild(cell);
    }
 }
 
 createGrid(16);
 
-function drawCell(cell) {
-   if (cell.style.opacity < 100) { 
-      cell.style.opacity += 10;
-   }
+
+
+function drawCell() {
+    cells = Array.from(cells)
+    cells.forEach ((cell) => {
+        cell.addEventListener("mouseenter", () => {
+        cell.style.opacity = "100%"
+        console.log("hi");
+        });
+    });
 }
 
-let cells = document.querySelectorAll("div", "cell");
-cells.forEach ((cell) => {
-   cell.addEventListener("mouseEnter", drawCell(cell));
-});
+let cells = document.getElementsByClassName("cell");
+drawCell();
 
 let createButton = document.getElementById("createButton");
 
@@ -31,17 +42,27 @@ let resetButton = document.getElementById("resetButton");
 
 createButton.addEventListener("click", () => {
    let num = prompt("Please enter a grid size between 1 and 100.");
+   num = parseInt(num);
    if (num < 1 || num > 100) {
       alert("Number outside of bounds!");
-      createGrid(16);
+   } else if (typeof num !== "number" || isNaN(num)) {
+    alert("Not a valid number!");
    }
    else {
-      createGrid(num);
+    console.log(num);
+    cells.forEach((cell) => {
+        cell.remove();
+    });
+    createGrid(num);
+    cells = document.getElementsByClassName("cell");
+    drawCell();
    }
 });
 
+
+
 resetButton.addEventListener("click", () =>  {
    cells.forEach ((cell) => {
-      cell.style.opacity = 0;
+      cell.style.opacity = "10%";
    });
 });
